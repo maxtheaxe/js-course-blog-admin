ac03f219b4c89e09e2a0380611e0b62f1804a9e2
Max Perrello <max.perrello@gmail.com>
2022-11-07 20:40:16 +0600

    ignore linting error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import EditPost from "./pages/posts/EditPost";
